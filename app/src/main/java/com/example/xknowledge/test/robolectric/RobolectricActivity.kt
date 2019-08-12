7531568f993f0bea860bfc2cb10e7edf306ab659
chengxiang.peng <chengxiang.peng@qunar.com>
2019-08-12 17:53:31 +0800

    添加Robolectric Demo

--- app/src/main/java/com/example/xknowledge/test/robolectric/RobolectricActivity.kt
+++ app/src/main/java/com/example/xknowledge/test/robolectric/RobolectricActivity.kt
@@ -1,6 +1,8 @@
 package com.example.xknowledge.test.robolectric
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import com.example.xknowledge.R
 import com.example.xknowledge.TitleActivity
 
@@ -9,5 +11,11 @@ class RobolectricActivity : TitleActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_robolectric)
+
+        val loginButton = findViewById<Button>(R.id.robolectric_button_login)
+        loginButton.setOnClickListener {
+            val intent = Intent(this@RobolectricActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
