023f1175072c0c038e0dbbc25d9de83c83031a20
ChengXiang.Peng <106783860@qq.com>
2019-09-15 18:24:12 +0800

    添加Java模块，和反射动态代理代码——补充提交2

--- java/src/main/java/com/java/xknowledge/reflect/proxy/Main.java
+++ java/src/main/java/com/java/xknowledge/reflect/proxy/Main.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Proxy;
 public class Main {
     public static void main(String[] args) {
         //动态代理，在不修改源码的情况下，增强一些方法，在方法执行前后执行任何事情
-        //FIXME 跟装饰着模式的区别是，或者封装方法前后执行相关不就可以了？？
+        //一般用在一些Java框架，组件化之间解耦实现代理模式
         RealSubject real = new RealSubject();
         Subject proxySubject = (Subject) Proxy.newProxyInstance(Subject.class.getClassLoader(), new Class[]{Subject.class}, new ProxyHandler(real));
         proxySubject.doSomeThings();
