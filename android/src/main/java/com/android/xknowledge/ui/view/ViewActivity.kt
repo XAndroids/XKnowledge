1f84899fa62277517993380ffd7fe33a22fd4bee
ChengXiang.Peng <106783860@qq.com>
2019-11-21 12:15:22 +0800

    完成ViewStub组件使用

--- android/src/main/java/com/android/xknowledge/ui/view/ViewActivity.kt
+++ android/src/main/java/com/android/xknowledge/ui/view/ViewActivity.kt
@@ -5,6 +5,7 @@ import com.android.xknowledge.ui.view.covered.CoveredActivity
 import com.android.xknowledge.ui.view.dialog.DialogActivity
 import com.android.xknowledge.ui.view.recyclerview.RecyclerViewActivity
 import com.android.xknowledge.ui.view.viewpager.ViewPagerCacheActivity
+import com.android.xknowledge.ui.view.viewstub.ViewStubActivity
 
 class ViewActivity : ListActivity() {
     override fun getMyListItemList(): List<ListItem> {
@@ -12,7 +13,8 @@ class ViewActivity : ListActivity() {
             ListItem("RecyclerView", "RecyclerView相关", RecyclerViewActivity::class.java),
             ListItem("Dialog", "对话框相关", DialogActivity::class.java),
             ListItem("ViewPager", "ViewPager缓存相关", ViewPagerCacheActivity::class.java),
-            ListItem("Covered", "View遮挡检测相关", CoveredActivity::class.java)
+            ListItem("Covered", "View遮挡检测相关", CoveredActivity::class.java),
+            ListItem("ViewStub", "ViewStub使用相关", ViewStubActivity::class.java)
         )
     }
 }
