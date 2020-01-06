39f18f596cd76b9d999697ef092bdd29ee1db4ec
ChengXiang.Peng <106783860@qq.com>
2020-01-06 14:49:14 +0800

    完成Fresco的基本接入

--- android/src/main/java/com/android/xknowledge/XApplication.kt
+++ android/src/main/java/com/android/xknowledge/XApplication.kt
@@ -2,10 +2,16 @@ package com.android.xknowledge
 
 import android.app.Activity
 import android.app.Application
+import com.facebook.drawee.backends.pipeline.Fresco
 
 
 class XApplication : Application() {
     companion object {
         var sLeakyActivities = ArrayList<Activity>()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Fresco.initialize(this)
     }
 }
--- android/src/main/java/com/android/xknowledge/framework/FrameworkActivity.kt
+++ android/src/main/java/com/android/xknowledge/framework/FrameworkActivity.kt
@@ -2,12 +2,13 @@ package com.android.xknowledge.framework
 
 import com.android.xknowledge.ListActivity
 import com.android.xknowledge.framework.eventbus.EventbusActivity
+import com.android.xknowledge.framework.fresco.FrescoActivity
 
 class FrameworkActivity : ListActivity() {
     override fun getMyListItemList(): List<ListItem> {
         return listOf(
-            ListItem("EventBus", "EventBus使用", EventbusActivity::class.java)
+            ListItem("EventBus", "EventBus使用", EventbusActivity::class.java),
+            ListItem("Fresco", "Fresco请求取消", FrescoActivity::class.java)
         )
     }
-
 }
