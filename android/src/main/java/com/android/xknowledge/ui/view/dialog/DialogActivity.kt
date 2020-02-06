d74b56d0a951a1841475cfa8a7b6e059a984554e
ChengXiang.Peng <106783860@qq.com>
2020-02-06 21:53:09 +0800

    蒙层和不带蒙层对话框实现

--- android/src/main/java/com/android/xknowledge/ui/view/dialog/DialogActivity.kt
+++ android/src/main/java/com/android/xknowledge/ui/view/dialog/DialogActivity.kt
@@ -7,13 +7,17 @@ class DialogActivity : ListActivity() {
         return listOf(
             ListItem(
                 "WebViewDialog",
-                "实现在全屏完全透明的DialogFragment中，嵌套全屏完全透明的WebView",
+                "实现在全屏透明的DialogFragment中，嵌套全屏透明的WebView",
                 DialogWebviewActivity::class.java
             ),
             ListItem(
                 "DialogCheck",
                 "实现当前页面是否存在Dialog展示",
                 DialogCheckActivity::class.java
+            ), ListItem(
+                "DialogTranslucent",
+                "实现Dialog有半透明蒙层和无蒙层样式",
+                DialogTranslucentActivity::class.java
             )
         )
     }
---
+++ android/src/main/java/com/android/xknowledge/ui/view/dialog/TranslucentDialog.kt
@@ -0,0 +1,34 @@
+package com.android.xknowledge.ui.view.dialog
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.KeyEvent
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import com.android.xknowledge.R
+
+class TranslucentDialog : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = activity?.let {
+            val builder = AlertDialog.Builder(it)
+            val inflater = requireActivity().layoutInflater;
+            builder.setView(inflater.inflate(R.layout.dialog_translucent, null))
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+
+//        dialog.let {
+//            //取消点击，点击外部和Back按键关闭对话框
+//            //参考：https://www.jianshu.com/p/b6b89eda6904
+//            it.setCancelable(false)
+//            it.setCanceledOnTouchOutside(false)
+//            it.setOnKeyListener { _, keyCode, _ -> keyCode == KeyEvent.KEYCODE_BACK }
+//        }
+
+        return dialog
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog.window.setBackgroundDrawableResource(android.R.color.transparent)
+    }
+}
