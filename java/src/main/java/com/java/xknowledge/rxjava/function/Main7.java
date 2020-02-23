3fea2fbb9f7079eac916e79c71d92d185c78d146
ChengXiang.Peng <106783860@qq.com>
2020-02-23 10:27:37 +0800

    完成RxJava功能操作符Demo

---
+++ java/src/main/java/com/java/xknowledge/rxjava/function/Main7.java
@@ -0,0 +1,45 @@
+package com.java.xknowledge.rxjava.function;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+//retry()，参考：https://www.jianshu.com/p/b0c3669affdb
+public class Main7 {
+    public static void main(String[] args) {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onError(new Exception("发生错误了"));
+                emitter.onNext(3);
+            }
+
+        }).retry()// 遇到错误时，让被观察者重新发射数据（若一直错误，则一直重新发送
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        System.out.println("开始采用subscribe连接");
+                    }
+
+                    @Override
+                    public void onNext(Integer value) {
+                        System.out.println("对Next事件 = " + value + " 作出响应");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        System.out.println("对Error事件作出响应");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("对Complete事件作出响应");
+                    }
+                });
+
+    }
+}
