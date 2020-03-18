e6956f6a2b7b20b67cad90fa1365e9949beb129c
ChengXiang.Peng <106783860@qq.com>
2020-03-18 19:50:37 +0800

    沉浸式相关功能测试

--- android/src/main/java/com/android/xknowledge/ui/UiActivity.kt
+++ android/src/main/java/com/android/xknowledge/ui/UiActivity.kt
@@ -4,6 +4,7 @@ import com.android.xknowledge.ListActivity
 import com.android.xknowledge.ui.dialog.DialogActivity
 import com.android.xknowledge.ui.event.EventActivity
 import com.android.xknowledge.ui.link.LinkActivity
+import com.android.xknowledge.ui.statusbar.StatusBarActivity
 import com.android.xknowledge.ui.view.ViewActivity
 
 class UiActivity : ListActivity() {
@@ -12,7 +13,8 @@ class UiActivity : ListActivity() {
             ListItem("View", "视图和组件使用相关", ViewActivity::class.java),
             ListItem("Dialog", "对话框相关", DialogActivity::class.java),
             ListItem("Event", "点击和滑动等交互相关", EventActivity::class.java),
-            ListItem("Link", "跳转链接相关", LinkActivity::class.java)
+            ListItem("Link", "跳转链接相关", LinkActivity::class.java),
+            ListItem("StatusBar", "状态栏相关", StatusBarActivity::class.java)
         )
     }
 }
