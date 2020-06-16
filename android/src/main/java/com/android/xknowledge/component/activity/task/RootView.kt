cbb8327c9f7c6292cc72d31b25a8aee2a60953cf
ChengXiang.Peng <106783860@qq.com>
2020-06-16 14:16:08 +0800

    实现在Activity的ViewGroup中获取Activity栈栈顶的信息

---
+++ android/src/main/java/com/android/xknowledge/component/activity/task/RootView.kt
@@ -0,0 +1,46 @@
+package com.android.xknowledge.component.activity.task
+
+import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
+import android.content.ContextWrapper
+import android.util.AttributeSet
+import android.util.Log
+import android.widget.FrameLayout
+import java.util.*
+
+
+class RootView(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
+    init {
+        val timerTask: TimerTask = object : TimerTask() {
+            override fun run() {
+                val activityClassName: String = getActivity()?.javaClass?.name ?: ""
+                Log.i("RootView", "activityClassName = $activityClassName")
+                val topActivityClassName = getTopActivity(context)
+                Log.i("RootView", "topActivityClassName = $topActivityClassName")
+            }
+        }
+        Timer().schedule(timerTask, 0, 1000)
+    }
+
+    private fun getActivity(): Activity? {
+        var context = context
+        while (context is ContextWrapper) {
+            if (context is Activity) {
+                return context
+            }
+            context = context.baseContext
+        }
+        return null
+    }
+
+    fun getTopActivity(context: Context): String? {
+        val am =
+            context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        val list = am.getRunningTasks(1)
+        if (list != null && list.size > 0) {
+            return list[0].topActivity?.className
+        }
+        return ""
+    }
+}
---
+++ android/src/main/java/com/android/xknowledge/component/activity/task/TaskActivity.kt
@@ -0,0 +1,23 @@
+package com.android.xknowledge.component.activity.task
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.android.xknowledge.R
+import com.android.xknowledge.component.activity.ActivityActivity
+
+/**
+ * 实现在ViewGroup中获取当前Activity栈的栈顶的相关信息
+ */
+class TaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task)
+        findViewById<Button>(R.id.taskactivity_button_next).setOnClickListener {
+            val intent = Intent(this@TaskActivity, ActivityActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
