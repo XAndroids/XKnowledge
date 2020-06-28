c58dd34536cadb1c0be83467830ece1b7826afe2
ChengXiang.Peng <106783860@qq.com>
2020-06-29 01:08:35 +0800

    merge标签和原理

--- android/src/main/java/com/android/xknowledge/ui/layout/LayoutActivity.kt
+++ android/src/main/java/com/android/xknowledge/ui/layout/LayoutActivity.kt
@@ -5,7 +5,8 @@ import com.android.xknowledge.ListActivity
 class LayoutActivity : ListActivity() {
     override fun getMyListItemList(): List<ListItem> {
         return listOf(
-            ListItem("include/merge", "include/merge标签相关", IncludeActivity::class.java)
+            ListItem("include", "include标签相关", IncludeActivity::class.java),
+            ListItem("merge", "merge标签相关", MergeActivity::class.java)
         )
     }
 }
