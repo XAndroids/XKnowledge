d5ffbc70518d371fb1be7f0eddca601960367efc
ChengXiang.Peng <106783860@qq.com>
2020-06-28 22:31:38 +0800

    iconfont实践

--- android/src/main/java/com/android/xknowledge/ui/view/textview/TextViewActivity.kt
+++ android/src/main/java/com/android/xknowledge/ui/view/textview/TextViewActivity.kt
@@ -6,7 +6,8 @@ class TextViewActivity : ListActivity() {
     override fun getMyListItemList(): List<ListItem> {
         return listOf(
             ListItem("LineHeight", "文案高度相关", LineHeightActivity::class.java),
-            ListItem("Ellipsize", "文案省略相关", EllipsizeActivity::class.java)
+            ListItem("Ellipsize", "文案省略相关", EllipsizeActivity::class.java),
+            ListItem("IconFont", "IconFont相关", IconFontActivity::class.java)
         )
     }
 }
