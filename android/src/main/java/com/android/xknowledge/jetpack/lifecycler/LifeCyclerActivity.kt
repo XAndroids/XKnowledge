9d99bb6d7d9ffaf43ee31c82839932ed27eea5c0
ChengXiang.Peng <106783860@qq.com>
2020-08-02 16:18:12 +0800

    添加LifeCycler补充

--- android/src/main/java/com/android/xknowledge/jetpack/lifecycler/LifeCyclerActivity.kt
+++ android/src/main/java/com/android/xknowledge/jetpack/lifecycler/LifeCyclerActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import com.android.xknowledge.R
 import com.android.xknowledge.TitleActivity
 
+/**
+ * 参考：https://chiclaim.blog.csdn.net/article/details/104189041
+ */
 class LifeCyclerActivity : TitleActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
