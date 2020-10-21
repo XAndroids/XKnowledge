6f4c9825d0d8f37a54bc5a1dc3be22c6b924bf4b
ChengXiang.Peng <106783860@qq.com>
2020-10-21 22:45:27 +0800

    集合ArrayList，LinkedList和HashMap性能测试

---
+++ java/src/main/java/com/java/xknowledge/se/collection/CollectionPerformanceTest.java
@@ -0,0 +1,77 @@
+package com.java.xknowledge.se.collection;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+/**
+ * 集合性能测试
+ */
+class CollectionPerformanceTest {
+    //顺序表
+    static ArrayList<Student> arrayList = new ArrayList<>();
+    //链表
+    static LinkedList<Student> linkedList = new LinkedList<>();
+    //HashMap
+    static HashMap<Integer, Student> hashMap = new HashMap<>();
+
+    public static void main(String[] args) {
+        //从集合中间增加数据，linkedList>hashMap>arrayList，hashMap最均衡
+        //arrayList insert time = 48
+        //linkedList insert time = 7
+        //hashMap insert time = 14
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            Student student = new Student(i, "student" + i);
+            arrayList.add(0, student);
+        }
+        System.out.println("arrayList insert time = " + (System.currentTimeMillis() - start));
+
+        long start1 = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            Student student = new Student(i, "student" + i);
+            linkedList.addFirst(student);
+        }
+        System.out.println("linkedList insert time = " + (System.currentTimeMillis() - start1));
+
+        long start2 = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            Student student = new Student(i, "student" + i);
+            hashMap.put(student.id, student);
+        }
+        System.out.println("hashMap insert time = " + (System.currentTimeMillis() - start2));
+
+
+        //从集合中遍历获取数据，arrayList>hashMap>linkedList，hashMap最均衡
+        //arrayList get time = 3
+        //linkedList get time = 670
+        //hashMap get time = 8
+        long start4 = System.currentTimeMillis();
+        for (int i = 0; i < arrayList.size(); i++) {
+            arrayList.get(i);
+        }
+        System.out.println("arrayList get time = " + (System.currentTimeMillis() - start4));
+
+        long start5 = System.currentTimeMillis();
+        for (int i = 0; i < linkedList.size(); i++) {
+            linkedList.get(i);
+        }
+        System.out.println("linkedList get time = " + (System.currentTimeMillis() - start5));
+
+        long start6 = System.currentTimeMillis();
+        for (int i = 0; i < hashMap.size(); i++) {
+            hashMap.get(i);
+        }
+        System.out.println("hashMap get time = " + (System.currentTimeMillis() - start6));
+    }
+
+    static class Student {
+        int id;
+        String name;
+
+        public Student(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}
