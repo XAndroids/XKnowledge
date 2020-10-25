8bdbab61eb4aff45c89b443a2b4c2590bc673ce2
ChengXiang.Peng <106783860@qq.com>
2020-10-25 22:11:47 +0800

    完成图的深度遍历实践

--- java/src/main/java/com/java/xknowledge/arithmetic/graph/cover/GraphCoverTest.java
+++ java/src/main/java/com/java/xknowledge/arithmetic/graph/cover/GraphCoverTest.java
@@ -1,7 +1,12 @@
 package com.java.xknowledge.arithmetic.graph.cover;
 
+/**
+ * 图的遍历
+ * 参考：《享学1：12图论（中），图的搜索，深度和广度优先方式如何遍历整个图》
+ */
 class GraphCoverTest {
     public static void main(String[] args) {
+        //深度遍历
         GraphCover graphCover = new GraphCover();
         graphCover.DeepFirst(0);
     }
