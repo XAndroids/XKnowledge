f2171dc6d2e16b3605e109e2b14601cf8c298ec9
ChengXiang.Peng <106783860@qq.com>
2020-11-12 01:48:58 +0800

    完成JNI静态注册实践

---
+++ java/src/main/java/com/java/xknowledge/se/jni/Register.java
@@ -0,0 +1,18 @@
+package com.java.xknowledge.se.jni;
+
+/**
+ * JNI静态注册
+ * 参考： 享学一期《JNI开发之JNI编译与运行》
+ */
+class Register {
+    static {
+        System.load("/Users/qitmac0000562/CLionProjects/CPlusKnowledge/cmake-build-debug" +
+                "/libfirstLib.dylib");
+    }
+    public native String HelloWorld();
+
+    public static void main(String[] args) {
+        Register register = new Register();
+        System.out.print(register.HelloWorld());
+    }
+}
