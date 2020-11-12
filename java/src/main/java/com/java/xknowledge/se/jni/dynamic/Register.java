d3d4cdb3abf430f29b84106496ee1407f986694b
ChengXiang.Peng <106783860@qq.com>
2020-11-13 01:32:02 +0800

    完成Java JNI 动态注册

--- java/src/main/java/com/java/xknowledge/se/jni/dynamic/Register.java
+++ java/src/main/java/com/java/xknowledge/se/jni/dynamic/Register.java
@@ -1,9 +1,26 @@
 package com.java.xknowledge.se.jni.dynamic;
 
+/**
+ * JNI动态注册
+ * 参考： 享学一期《JNI开发之JNI编译与运行》
+ */
 class Register {
-    public native void dynamicFunc1();
+    static {
+        System.load("/Users/qitmac0000562/CLionProjects/CPlusKnowledge/cmake-build-debug" +
+                "/libfirstdyLib.dylib");
+    }
 
-    static{
-        System.load();
+    //动态注册方法
+    native void dynamicFunc1();
+
+    native String dynamicFunc2();
+
+    native int getRandom();
+
+    public static void main(String[] args) {
+        Register register = new Register();
+        register.dynamicFunc1();
+        System.out.println(register.dynamicFunc2());
+        System.out.println(register.getRandom());
     }
 }
