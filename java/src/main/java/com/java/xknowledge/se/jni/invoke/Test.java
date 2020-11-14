80f0d910f8f85fa932365e3235921499c1ddf041
ChengXiang.Peng <106783860@qq.com>
2020-11-14 22:02:56 +0800

    完善了JNI属性的访问

--- java/src/main/java/com/java/xknowledge/se/jni/invoke/Test.java
+++ java/src/main/java/com/java/xknowledge/se/jni/invoke/Test.java
@@ -4,6 +4,8 @@ package com.java.xknowledge.se.jni.invoke;
  * C/C++调用Java代码签名
  */
 public class Test {
+    public int property = 1;
+
     //Test的PrintfTest方法提供给C/C++中调用
     public static void PrintfStaticTest() {
         System.out.println("PrintfStaticTest Invoke");
