cc99c3681b6f3eef73fae707f69ead85e21793b9
ChengXiang.Peng <106783860@qq.com>
2020-11-23 17:30:20 +0800

    泛型作用实践

---
+++ java/src/main/java/com/java/xknowledge/se/generics/GenericsTest.java
@@ -0,0 +1,56 @@
+package com.java.xknowledge.se.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 泛型作用时间类：
+ * 1.避免类型转换；
+ * 2.运行时异常提前到编译期；
+ * 参考：https://zhuanlan.zhihu.com/p/28242753
+ */
+class GenericsTest {
+    public static void main0(String[] args) {
+        //泛型作用1：避免强制转换
+        //Raw use of parameterized class 'ArrayList'
+        List list = new ArrayList();
+        //Unchecked call to 'add(E)' as a member of raw type 'java.util.List'
+        list.add("abc");
+        String s = (String) list.get(0);
+        System.out.println("s = " + s);
+
+        //使用泛型后,保存入和取出的是String类型，故不需要类型转换
+        List<String> list2 = new ArrayList<>();
+        list2.add("abc");
+        String s2 = list2.get(0);
+        System.out.println("s2 = " + s2);
+    }
+
+    public static void main(String[] args) {
+        //泛型作用2：将运行时错误提前到编译时
+        //string = abctest
+        //Exception in thread "main" java.lang.ClassCastException: java.lang.Integer cannot be cast to java.lang.String
+        //	at com.java.xknowledge.se.generics.GenericsTest.main(GenericsTest.java:33)
+        //因为没有使用泛型，object都可以保存在list中，但后续运行时强制类型转换会抛异常
+//        List list = new ArrayList();
+//        list.add("abc");
+//        list.add(1);
+//        for (int i = 0; i < list.size(); i++) {
+//            StringBuffer string = new StringBuffer((String) list.get(i));
+//            string.append("test");
+//            System.out.println("string = " + string);
+//        }
+
+        List<String> list2 = new ArrayList<>();
+        list2.add("abc");
+        //使用泛型后，编译时进行类型检查，如果不匹配则抛出异常
+        //generics/GenericsTest.java:43: 错误: 对于add(int), 找不到合适的方法
+        //        list2.add(1);
+        //list2.add(1);
+        for (int i = 0; i < list2.size(); i++) {
+            StringBuffer string2 = new StringBuffer((String) list2.get(i));
+            string2.append("test");
+            System.out.println("string = " + string2);
+        }
+    }
+}
