c25d7547972ab4f6f94ba03fd61c1f48a4cdc851
ChengXiang.Peng <106783860@qq.com>
2020-11-29 23:49:01 +0800

    泛型擦除实践

---
+++ java/src/main/java/com/java/xknowledge/se/generics/erasure/Erasure.java
@@ -0,0 +1,57 @@
+package com.java.xknowledge.se.generics.erasure;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * 泛型擦除
+ * 1.无限制类型擦除，为Object类型；
+ * 2.有限制类型擦除，为上界类型；
+ * 3.泛型方法擦除，为上界类型；
+ * 参考：https://www.bilibili.com/video/BV1xJ411n77R?p=11
+ */
+class Erasure<T, K extends Number> {
+    private T key;//无限制类型泛型，
+    private K value;//有限制类型泛型
+
+    public T getKey() {
+        return key;
+    }
+
+    public void setKey(T key) {
+        this.key = key;
+    }
+
+    public K getInfo() {
+        return value;
+    }
+
+    public void setValue(K value) {
+        this.value = value;
+    }
+
+    //泛型方法类型
+    public <V extends Number> V getInfo(V info) {
+        return info;
+    }
+
+    public static void main(String[] args) {
+        Erasure<Integer, Number> integerErasure = new Erasure<>();
+        Class<? extends Erasure> clz = integerErasure.getClass();
+        Field[] declaredField = clz.getDeclaredFields();
+        //类型擦除后，key无限制类型擦除为Object，value为有限制类型擦除为上界
+        //key:Object
+        //value:Number
+        for (Field field : declaredField) {
+            System.out.println(field.getName() + ":" + field.getType().getSimpleName());
+        }
+        System.out.println();
+        Method[] declaredMethod = clz.getMethods();
+        //泛型方法擦除为上界
+        //getInfo:Number
+        //getInfo:Number
+        for (Method method : declaredMethod) {
+            System.out.println(method.getName() + ":" + method.getReturnType().getSimpleName());
+        }
+    }
+}
