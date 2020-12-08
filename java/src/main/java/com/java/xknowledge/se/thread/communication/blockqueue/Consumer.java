aa6e704f35c63ff6dbcefd06ce14f1995935bbf0
ChengXiang.Peng <106783860@qq.com>
2020-12-08 12:29:06 +0800

    线程通信BlockingQueue，增加注释

--- java/src/main/java/com/java/xknowledge/se/thread/communication/blockqueue/Consumer.java
+++ java/src/main/java/com/java/xknowledge/se/thread/communication/blockqueue/Consumer.java
@@ -2,6 +2,9 @@ package com.java.xknowledge.se.thread.communication.blockqueue;
 
 import java.util.concurrent.BlockingQueue;
 
+/**
+ * 消费者线程，从BlockingQueue中获取String，如果队列为空则阻塞当前线程，等待生产者生产String在唤醒执行
+ */
 class Consumer extends Thread {
     private BlockingQueue<String> blockingQueue;
 
@@ -15,7 +18,7 @@ class Consumer extends Thread {
             System.out.println(getName() + "消费者准备消费集合元素！");
             try {
                 Thread.sleep(200);
-                blockingQueue.take();
+                blockingQueue.take();//如果队列为空，则阻塞消费者线程，等待生产者生产
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
--- java/src/main/java/com/java/xknowledge/se/thread/communication/blockqueue/Producer.java
+++ java/src/main/java/com/java/xknowledge/se/thread/communication/blockqueue/Producer.java
@@ -2,6 +2,10 @@ package com.java.xknowledge.se.thread.communication.blockqueue;
 
 import java.util.concurrent.BlockingQueue;
 
+/**
+ * 生产者线程，从strArr中生产字符串，加入到BlockingQueue，如果Blocking满了则阻塞当前生产者线程，等待消费者线程
+ * 消费后，在唤醒生产者生产
+ */
 class Producer extends Thread {
     private BlockingQueue<String> blockingQueue;
 
@@ -16,7 +20,7 @@ class Producer extends Thread {
             System.out.println(getName() + "生产者准备生产集合元素！");
             try {
                 Thread.sleep(200);
-                blockingQueue.put(strArr[i % 3]);
+                blockingQueue.put(strArr[i % 3]);//生产字符串，如果队列满了则阻塞线程
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
