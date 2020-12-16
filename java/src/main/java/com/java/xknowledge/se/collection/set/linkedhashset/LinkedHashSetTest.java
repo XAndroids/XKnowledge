4dccf802ec93ef259b9ee0e21421e633a2c60d02
ChengXiang.Peng <106783860@qq.com>
2020-12-16 22:32:04 +0800

    LinkedHashSetTest保证元素插入顺序2

--- java/src/main/java/com/java/xknowledge/se/collection/set/linkedhashset/LinkedHashSetTest.java
+++ java/src/main/java/com/java/xknowledge/se/collection/set/linkedhashset/LinkedHashSetTest.java
@@ -19,6 +19,7 @@ class LinkedHashSetTest {
         books.add("疯狂Java讲义");
         books.add("轻量级Java EE企业应用实战");
         System.out.println(books);
+
         //删除 疯狂Java讲义
         books.remove("疯狂Java讲义");
         //重新添加 疯狂Java讲义
