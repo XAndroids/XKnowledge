1acfcf8e62644c915cf852138da81a01d4a530f9
ChengXiang.Peng <106783860@qq.com>
2020-12-16 20:46:25 +0800

    TreeSet实践2

---
+++ java/src/main/java/com/java/xknowledge/se/collection/set/TreeSetErrorTest.java
@@ -0,0 +1,26 @@
+package com.java.xknowledge.se.collection.set;
+
+import java.util.TreeSet;
+
+/**
+ * TreeSet：添加元素必须时间Comparable接口
+ * 运行：
+ * Exception in thread "main" java.lang.ClassCastException: com.java.xknowledge.se.collection.set.Err cannot be cast to java.lang.Comparable
+ * 	at java.util.TreeMap.compare(TreeMap.java:1294)
+ * 	at java.util.TreeMap.put(TreeMap.java:538)
+ * 	at java.util.TreeSet.add(TreeSet.java:255)
+ * 	at com.java.xknowledge.se.collection.set.TreeSetErrorTest.main(TreeSetErrorTest.java:9)
+ * 参考：
+ * 《疯狂Java讲义》
+ */
+public class TreeSetErrorTest {
+    public static void main(String[] args) {
+        TreeSet ts = new TreeSet();
+        //向TreeSet集合中添加两个Err对象
+        ts.add(new Err());
+        ts.add(new Err());  //①
+    }
+}
+
+class Err {
+}
