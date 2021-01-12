2a57d313d2c9dbe379779b7ed399eeff83b8fb3c
ChengXiang.Peng <106783860@qq.com>
2021-01-12 11:17:17 +0800

    Leakcanary接入

--- android/src/main/java/com/android/xknowledge/optimize/leakcanary/LeakcanaryActivity.java
+++ android/src/main/java/com/android/xknowledge/optimize/leakcanary/LeakcanaryActivity.java
@@ -1,29 +1,24 @@
 package com.android.xknowledge.optimize.leakcanary;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import com.android.xknowledge.R;
+import com.android.xknowledge.TitleActivity;
 
 /**
  * Leakcanary的使用和原理
  * 参考：https://juejin.im/post/6844903924747468813
  */
-public class LeakcanaryActivity extends AppCompatActivity {
+public class LeakcanaryActivity extends TitleActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leakcanary);
-        findViewById(R.id.leakcanary_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LeakcanaryActivity.this, TestActivity.class);
-                startActivity(intent);
-            }
+        findViewById(R.id.leakcanary_button).setOnClickListener(v -> {
+            Intent intent = new Intent(LeakcanaryActivity.this, TestActivity.class);
+            startActivity(intent);
         });
 
     }
