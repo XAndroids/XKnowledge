a2894d24423cb78a7fd0e017f1264268e48ede28
ChengXiang.Peng <106783860@qq.com>
2021-01-16 22:40:37 +0800

    Systrace API实践

--- android/src/main/java/com/android/xknowledge/optimize/OptimizeActivity.kt
+++ android/src/main/java/com/android/xknowledge/optimize/OptimizeActivity.kt
@@ -5,6 +5,7 @@ import com.android.xknowledge.optimize.gc.GcActivity
 import com.android.xknowledge.optimize.leakcanary.LeakcanaryActivity
 import com.android.xknowledge.optimize.leakmemory.LeakMemoryActivity
 import com.android.xknowledge.optimize.strictmode.StrictActivity
+import com.android.xknowledge.optimize.systrace.SystraceActivity
 
 class OptimizeActivity : ListActivity() {
     override fun getMyListItemList(): List<ListItem> {
@@ -12,7 +13,8 @@ class OptimizeActivity : ListActivity() {
             ListItem("StrictActivity", "StrictMode相关", StrictActivity::class.java),
             ListItem("LeakcanaryActivity", "Leakcanary相关", LeakcanaryActivity::class.java),
             ListItem("GcActivity", "GC日志", GcActivity::class.java),
-            ListItem("LeakMemoryActivity", "内存泄露相关", LeakMemoryActivity::class.java)
+            ListItem("LeakMemoryActivity", "内存泄露相关", LeakMemoryActivity::class.java),
+            ListItem("SystraceActivity", "Systrace相关", SystraceActivity::class.java)
         )
     }
 }
---
+++ android/src/main/java/com/android/xknowledge/optimize/systrace/SystraceActivity.java
@@ -0,0 +1,35 @@
+package com.android.xknowledge.optimize.systrace;
+
+import android.os.Bundle;
+import androidx.core.os.TraceCompat;
+
+import com.android.xknowledge.R;
+import com.android.xknowledge.TitleActivity;
+
+/**
+ * Systrace API实践
+ * 参考：
+ * 享学2《性能优化-卡顿和布局优化》
+ */
+public class SystraceActivity extends TitleActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TraceCompat.beginSection("SystraceActivity");
+        setContentView(R.layout.activity_systrace);
+        for (int i = 0; i < 100; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            },"SystraceThread").start();
+        }
+        TraceCompat.endSection();
+    }
+}
