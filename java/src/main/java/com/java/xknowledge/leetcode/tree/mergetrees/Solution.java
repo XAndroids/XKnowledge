4ac9fac7a0a91cd0a5db93f6b760a72aae1cc790
ChengXiang.Peng <106783860@qq.com>
2021-04-03 18:39:37 +0800

    617. 合并二叉树

---
+++ java/src/main/java/com/java/xknowledge/leetcode/tree/mergetrees/Solution.java
@@ -0,0 +1,85 @@
+package com.java.xknowledge.leetcode.tree.mergetrees;
+
+/**
+ * 617. 合并二叉树
+ * 给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+ * <p>
+ * 你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入:
+ * Tree 1                     Tree 2
+ * 1                         2
+ * / \                       / \
+ * 3   2                     1   3
+ * /                           \   \
+ * 5                             4   7
+ * 输出:
+ * 合并后的树:
+ * 3
+ * / \
+ * 4   5
+ * / \   \
+ * 5   4   7
+ * 注意: 合并必须从两个树的根节点开始。
+ * 链接：https://leetcode-cn.com/problems/merge-two-binary-trees
+ */
+class Solution {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * 递归实现：同时递归两个二叉树，递归退出条件：任意一个树节点为null（即少了一条腿）返回另一个节点的子树，跳出
+     * 递归。否则mrege节点，继续递归；
+     */
+    public static TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {//注意递归退出条件！任意一个数节点为空，后续就没必要递归了，把非空的子树拷贝即可！
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+
+        TreeNode mergeNode = new TreeNode();//否则两个都不为null，则Merge节点
+        mergeNode.val = root1.val + root2.val;
+
+        mergeNode.left = mergeTrees(root1.left, root2.left);//递归合并当前节点的左子节点和右子节点
+        mergeNode.right = mergeTrees(root1.right, root2.right);
+
+        return mergeNode;
+    }
+
+    public static TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+
+    }
+
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(1);
+        root1.left = new TreeNode(2);
+        root1.left.left = new TreeNode(3);
+
+        TreeNode root2 = new TreeNode(1);
+        root2.right = new TreeNode(2);
+        root2.right.right = new TreeNode(2);
+
+        TreeNode resultRoot = mergeTrees(root1, root2);
+        System.out.println(resultRoot.toString());
+    }
+}
