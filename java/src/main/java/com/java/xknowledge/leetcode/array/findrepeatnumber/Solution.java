26adf62964e8ab6ffa921abd6e1a6e67fb508128
ChengXiang.Peng <106783860@qq.com>
2021-04-04 21:24:05 +0800

    剑指 Offer 03. 数组中重复的数字

---
+++ java/src/main/java/com/java/xknowledge/leetcode/array/findrepeatnumber/Solution.java
@@ -0,0 +1,24 @@
+package com.java.xknowledge.leetcode.array.findrepeatnumber;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 剑指 Offer 03. 数组中重复的数字
+ * 链接：https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
+ */
+class Solution {
+    public static int findRepeatNumber(int[] nums) {
+        Set<Integer> noRepeatSet = new HashSet<>();
+        for (int i = 0; i <= nums.length - 1; i++) {
+            if (!noRepeatSet.add(nums[i])) {//判断重复性，使用HashSet的不可重复特性
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findRepeatNumber(new int[]{3, 1, 2, 3}));
+    }
+}
