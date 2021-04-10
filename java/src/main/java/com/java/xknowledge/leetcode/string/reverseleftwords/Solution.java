c42db6a3dc23a7c14276b5f3d4b92f7286e0daa3
ChengXiang.Peng <106783860@qq.com>
2021-04-10 22:47:03 +0800

    剑指 Offer 58 - II. 左旋转字符串

---
+++ java/src/main/java/com/java/xknowledge/leetcode/string/reverseleftwords/Solution.java
@@ -0,0 +1,14 @@
+package com.java.xknowledge.leetcode.string.reverseleftwords;
+
+/**
+ * 剑指 Offer 58 - II. 左旋转字符串
+ * 链接：https://leetcode-cn.com/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof/
+ */
+class Solution {
+    /**
+     * 字符串拼接
+     */
+    public String reverseLeftWords(String s, int n) {
+        return s.substring(n) + s.substring(0, n);
+    }
+}
