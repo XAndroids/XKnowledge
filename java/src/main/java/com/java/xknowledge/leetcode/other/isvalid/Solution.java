c45dff87c088c9bf3ce49fe5dcfc4b2cdd4ff044
ChengXiang.Peng <106783860@qq.com>
2021-04-10 18:16:15 +0800

    调整代码

--- java/src/main/java/com/java/xknowledge/leetcode/other/isvalid/Solution.java
+++ java/src/main/java/com/java/xknowledge/leetcode/other/isvalid/Solution.java
@@ -6,41 +6,7 @@ import java.util.LinkedList;
 import java.util.Map;
 
 /**
- * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
- * <p>
- * 有效字符串需满足：
- * <p>
- * 左括号必须用相同类型的右括号闭合。
- * 左括号必须以正确的顺序闭合。
- *  
- * <p>
- * 示例 1：
- * <p>
- * 输入：s = "()"
- * 输出：true
- * 示例 2：
- * <p>
- * 输入：s = "()[]{}"
- * 输出：true
- * 示例 3：
- * <p>
- * 输入：s = "(]"
- * 输出：false
- * 示例 4：
- * <p>
- * 输入：s = "([)]"
- * 输出：false
- * 示例 5：
- * <p>
- * 输入：s = "{[]}"
- * 输出：true
- *  
- * <p>
- * 提示：
- * <p>
- * 1 <= s.length <= 104
- * s 仅由括号 '()[]{}' 组成
- * <p>
+ * 20. 有效的括号
  * 链接：https://leetcode-cn.com/problems/valid-parentheses
  */
 public class Solution {
