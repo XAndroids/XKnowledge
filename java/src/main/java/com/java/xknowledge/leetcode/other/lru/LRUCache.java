108c70e27ddc826177c850888e34aad5e45b2230
ChengXiang.Peng <106783860@qq.com>
2021-04-17 01:12:42 +0800

    146. LRU 缓存机制

---
+++ java/src/main/java/com/java/xknowledge/leetcode/other/lru/LRUCache.java
@@ -0,0 +1,44 @@
+package com.java.xknowledge.leetcode.other.lru;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * 146. LRU 缓存机制
+ * 连接：https://leetcode-cn.com/problems/lru-cache/
+ * 题解：https://leetcode-cn.com/problems/lru-cache/solution/lruhuan-cun-ji-zhi-by-leetcode-solution/
+ */
+class LRUCache extends LinkedHashMap<Integer, Integer> {
+    int capacity;
+
+    public LRUCache(int capacity) {
+        super(capacity, 0.75f, true);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+    }
+
+    public static void main(String[] args) {
+        LRUCache lruCache = new LRUCache(2);
+        lruCache.put(1, 1);
+        lruCache.put(2, 2);
+        System.out.println(lruCache.get(1));
+        lruCache.put(3, 3);
+        System.out.println(lruCache.get(2));
+        lruCache.put(4, 4);
+        System.out.println(lruCache.get(1));
+        System.out.println(lruCache.get(3));
+        System.out.println(lruCache.get(4));
+    }
+}
